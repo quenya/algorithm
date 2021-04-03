592d9cb03634c012ffb9056d6e9125a2eda0f55b
이종현 <gear.yi@miracom.co.kr>
2021-04-03 18:50:38 +0900

    평범한 배낭 12865 문제ˆ

--- src/deep_theory/prop_12865.java
+++ src/deep_theory/prop_12865.java
@@ -6,51 +6,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class prop_12865 {
-    static class Mat {
-        int w;
-        int v;
-        public Mat(int w, int v) {
-            this.w = w;
-            this.v = v;
-        }
-    }
-    static int N;
-    static int K;
-    static Mat[] map;
-    static boolean[] visited;
-    static int max_v;
+    static int N, K;
+    static int dp[][], W[], V[];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
         N = Integer.parseInt(s[0]);
         K = Integer.parseInt(s[1]);
-        map = new Mat[N];
-        for (int i = 0; i < N; i++) {
+        W = new int[N + 1];
+        V = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            map[i] = new Mat(w, v);
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
         br.close();
 
-        visited = new boolean[N];
-        max_v = 0;
-        BT(visited, 0, 0);
-        System.out.println(max_v);
-    }
-
-    static void BT(boolean[] visited, int w, int v) {
-        if (max_v < v)
-            max_v = v;
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                Mat mat = map[i];
-                if (w+mat.w <= K) {
-                    visited[i] = true;
-                    BT(visited, w+mat.w, v+mat.v);
-                    visited[i] = false;
-                }
+        dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= W[i])
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
             }
         }
+        System.out.println(dp[N][K]);
     }
 }
